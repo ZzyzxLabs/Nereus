25547d24cb058023f0d498cd41a80cca2ed99d5d
creaper9487 <w0979563238@gmail.com>
2025-11-17 23:24:31 +0800

    changes in sh

--- src/nautilus-server/run.sh
+++ src/nautilus-server/run.sh
@@ -22,6 +22,13 @@ busybox ip link set dev lo up
 
 # Add a hosts record, pointing target site calls to local loopback
 echo "127.0.0.1   localhost" > /etc/hosts
+echo "127.0.0.64   aggregator.testnet.walrus.atalma.io" >> /etc/hosts
+echo "127.0.0.65   publisher.walrus-01.tududes.com" >> /etc/hosts
+
+
+
+
+
 
 
 
@@ -51,6 +58,13 @@ echo "$JSON_RESPONSE" | jq -r 'to_entries[] | "\(.key)=\(.value)"' > /tmp/kvpair
 
 # == ATTENTION: code should be generated here that added all hosts to forward traffic ===
 # Traffic-forwarder-block
+python3 /traffic_forwarder.py 127.0.0.64 443 3 8101 &
+python3 /traffic_forwarder.py 127.0.0.65 443 3 8102 &
+
+
+
+
+
 
 
 
