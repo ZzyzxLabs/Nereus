2438fee3763e0b3ab6c5c7e67aa2ca86ff6bea28
zou <leftrightch@gmail.com>
2025-11-24 07:25:59 +0800

    fix: wrong vsock

--- src/nautilus-server/run.sh
+++ src/nautilus-server/run.sh
@@ -41,6 +41,7 @@ echo "127.0.0.67   127.0.0.1" >> /etc/hosts
 
 
 
+
 # == ATTENTION: code should be generated here that parses allowed_endpoints.yaml and populate domains here ===
 
 cat /etc/hosts
@@ -58,10 +59,11 @@ echo "$JSON_RESPONSE" | jq -r 'to_entries[] | "\(.key)=\(.value)"' > /tmp/kvpair
 
 # == ATTENTION: code should be generated here that added all hosts to forward traffic ===
 # Traffic-forwarder-block
-python3 /traffic_forwarder.py 127.0.0.64 443 3 8000 &
-python3 /traffic_forwarder.py 127.0.0.65 443 3 8000 &
-python3 /traffic_forwarder.py 127.0.0.66 443 3 8000 &
-python3 /traffic_forwarder.py 127.0.0.67 443 3 8000 &
+python3 /traffic_forwarder.py 127.0.0.64 443 3 8101 &
+python3 /traffic_forwarder.py 127.0.0.65 443 3 8102 &
+python3 /traffic_forwarder.py 127.0.0.66 443 3 8103 &
+python3 /traffic_forwarder.py 127.0.0.67 443 3 8104 &
+
 
 
 
